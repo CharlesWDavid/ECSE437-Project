6b647184e74deebf0adffffb301ad93498e64539
charles-david <charleswd@hotmail.ca>
2022-11-30 16:34:59 -0500

    Add mocked functions for Controls props
    Change-Id: I4888b926b53b3f42b83cb881e7e1723f5661fa2b

--- src/components/__tests__/Controls.test.js
+++ src/components/__tests__/Controls.test.js
@@ -1,43 +1,51 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import Controls from "../Controls";
+
+beforeEach(() => {
+  render(
+    <Controls
+      resetArray={jest.fn()}
+      mergeSort={jest.fn()}
+      quickSort={jest.fn()}
+      heapSort={jest.fn()}
+    />
+  );
+});
+
+afterEach(cleanup);
 
 describe("Controls Unit Tests", () => {
   it("Controls should render", async () => {
-    render(<Controls />);
     const divElement = screen.getByTestId("controls-testid");
     expect(divElement).toBeInTheDocument();
   });
 
   it("Controls should render with Quick Sort button", async () => {
-    render(<Controls />);
     const buttonElement = screen.getByRole("button", {
       name: "Quick Sort",
     });
     expect(buttonElement).toBeInTheDocument();
-  })
+  });
 
   it("Controls should render with Merge Sort button", async () => {
-    render(<Controls />);
     const buttonElement = screen.getByRole("button", {
       name: "Merge Sort",
     });
     expect(buttonElement).toBeInTheDocument();
-  })
+  });
 
   it("Controls should render with Heap Sort button", async () => {
-    render(<Controls />);
     const buttonElement = screen.getByRole("button", {
       name: "Heap Sort",
     });
     expect(buttonElement).toBeInTheDocument();
-  })
+  });
 
   it("Controls should render with Generate New Array button", async () => {
-    render(<Controls />);
     const buttonElement = screen.getByRole("button", {
       name: "Generate New Array",
     });
     expect(buttonElement).toBeInTheDocument();
-  })
+  });
 });
